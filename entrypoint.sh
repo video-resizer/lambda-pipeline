6ec8c78eb09f6797f2f5b50b2ff007e8df5bcd6b
Keith McDonald <keithmmcdonald@gmail.com>
2021-01-31 23:27:05 -0800

    Run TerraGrunt to deploy to staging upon successful test run

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,30 +17,31 @@ copy_params() {
     done
 }
 
-AWS_ACCESS_KEY_ID="${INPUT_AWS_ACCESS_KEY_ID}"
-AWS_SECRET_ACCESS_KEY="${INPUT_AWS_SECRET_ACCESS_KEY}"
-AWS_REGION="${INPUT_AWS_REGION}"
-PROGRAM_NAME="${INPUT_PROGRAM_NAME}"
-NEW_TAG="${INPUT_NEW_TAG}"
-TEST_DIR="${INPUT_TEST_DIR}"
-TEST_NAME="${INPUT_TEST_NAME}"
-
 # Update unit-test version in parameter store
-aws configure set aws_access_key_id "${AWS_ACCESS_KEY_ID}" || exit 1
-aws configure set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY}" || exit 1
-aws configure set region "${AWS_REGION}" || exit 1
+aws configure set aws_access_key_id "${INPUT_AWS_ACCESS_KEY_ID}" || exit 1
+aws configure set aws_secret_access_key "${INPUT_AWS_SECRET_ACCESS_KEY}" || exit 1
+aws configure set region "${INPUT_AWS_REGION}" || exit 1
 copy_params staging unit-test ssm-param || exit 1
 
-if [ -n "${PROGRAM_NAME}" ]; then
-    aws ssm put-parameter --name "/version/unit-test/${PROGRAM_NAME}" --type "String" --value "${NEW_TAG}" --overwrite || exit 1
+if [ -n "${INPUT_PROGRAM_NAME}" ]; then
+    aws ssm put-parameter --name "/version/unit-test/${INPUT_PROGRAM_NAME}" --type "String" --value "${INPUT_NEW_TAG}" --overwrite || exit 1
 fi
 
 # Run tests
-pushd "${GITHUB_WORKSPACE}"/"${TEST_DIR}"
-go test -count=1 -v -timeout 30m "${TEST_NAME}"
+pushd "${GITHUB_WORKSPACE}"/"${INPUT_TEST_DIR}"
+go test -count=1 -v -timeout 30m "${INPUT_TEST_NAME}"
 gotest_result=$?
 popd
 [ "${gotest_result}" -eq 0 ] || exit 1
 
 # Update staging version in parameter store
 copy_params unit-test staging ssm-param || exit 1
+
+# Deploy to staging
+if [ -n "${INPUT_LIVE_DIR}" ]; then
+    pushd "${GITHUB_WORKSPACE}"/"${INPUT_LIVE_DIR}"
+    terragrunt apply-all --terragrunt-non-interactive
+    terragrunt_result=$?
+    popd
+    [ "${terragrunt_result}" -eq 0 ] || exit 1
+fi
