d2d933efe0552de85e33a385c4f454b2ab4d1841
Keith McDonald <keithmmcdonald@gmail.com>
2021-02-02 00:15:45 -0800

    Write both version and sha to Parameter Store

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 copy_params() {
-    source="${1}"
-    destination="${2}"
-    profile="${3}"
+    type="${1}"
+    source="${2}"
+    destination="${3}"
 
-    eval "arr=( $(aws ssm get-parameters-by-path --path "/version/${source}/" | jq -r '@sh "\(.Parameters[].Name)"' ) )"
+    eval "arr=( $(aws ssm get-parameters-by-path --path "/${type}/${source}/" | jq -r '@sh "\(.Parameters[].Name)"' ) )"
 
     for key in "${arr[@]}"
     do
@@ -13,7 +13,7 @@ copy_params() {
         repo=$(echo "${noquotes}" | sed 's|.*/||')
         val=$(aws ssm get-parameter --name "${key}" | jq ".Parameter.Value")
         valnoquotes=$(echo "${val}" | sed 's/"//g')
-        aws ssm put-parameter --name "/version/${destination}/${repo}" --type "String" --value "${valnoquotes}" --overwrite
+        aws ssm put-parameter --name "/${type}/${destination}/${repo}" --type "String" --value "${valnoquotes}" --overwrite
     done
 }
 
@@ -22,13 +22,18 @@ function gotest(){
 }
 
 # Update unit-test version in parameter store
+archive_filename="${INPUT_PROGRAM_NAME}-${INPUT_NEW_TAG}.${INPUT_EXTENSION}"
+sha=$(sha256sum "${GITHUB_WORKSPACE}/${INPUT_BINARY_DIR}/${archive_filename}" | cut -d " " -f1)
+
 aws configure set aws_access_key_id "${INPUT_AWS_ACCESS_KEY_ID}" || exit 1
 aws configure set aws_secret_access_key "${INPUT_AWS_SECRET_ACCESS_KEY}" || exit 1
 aws configure set region "${INPUT_AWS_REGION}" || exit 1
-copy_params staging unit-test ssm-param || exit 1
+copy_params version staging unit-test || exit 1
+copy_params sha staging unit-test || exit 1
 
 if [ -n "${INPUT_PROGRAM_NAME}" ]; then
     aws ssm put-parameter --name "/version/unit-test/${INPUT_PROGRAM_NAME}" --type "String" --value "${INPUT_NEW_TAG}" --overwrite || exit 1
+    aws ssm put-parameter --name "/sha/unit-test/${INPUT_PROGRAM_NAME}" --type "String" --value "${sha}" --overwrite || exit 1
 fi
 
 # Run tests
@@ -43,7 +48,8 @@ popd
 [ "${gotest_result}" -eq 0 ] || exit 1
 
 # Update staging version in parameter store
-copy_params unit-test staging ssm-param || exit 1
+copy_params version unit-test staging || exit 1
+copy_params sha unit-test staging || exit 1
 
 # Deploy to staging
 if [ -n "${INPUT_LIVE_DIR}" ]; then
