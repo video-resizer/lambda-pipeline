263d4a215357d758d1a29ecc9738e566d3f33348
Keith McDonald <keithmmcdonald@gmail.com>
2021-02-03 21:53:36 -0800

    Only copy SSM parameters that have changed and, when they have changed, also copy the archive file from bucket to bucket

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ copy_params() {
     type="${1}"
     source="${2}"
     destination="${3}"
+    bucketprefix="${4}"
+    filename="${5}"
 
     eval "arr=( $(aws ssm get-parameters-by-path --path "/${type}/${source}/" | jq -r '@sh "\(.Parameters[].Name)"' ) )"
 
@@ -11,9 +13,13 @@ copy_params() {
     do
         noquotes=$(echo "${key}" | sed 's/"//g')
         repo=$(echo "${noquotes}" | sed 's|.*/||')
-        val=$(aws ssm get-parameter --name "${key}" | jq ".Parameter.Value")
-        valnoquotes=$(echo "${val}" | sed 's/"//g')
-        aws ssm put-parameter --name "/${type}/${destination}/${repo}" --type "String" --value "${valnoquotes}" --overwrite
+        sourceval=$(aws ssm get-parameter --name "${key}" | jq ".Parameter.Value")
+        destinationval=$(aws ssm get-parameter --name "/${type}/${destination}/${repo}" | jq ".Parameter.Value")
+        if [ "${sourceval}" != "${destinationval}" ]; then
+            sourcevalnoquotes=$(echo "${sourceval}" | sed 's/"//g')
+            aws s3 sync "s3://${bucketprefix}-${source}/${filename}" "s3://${bucketprefix}-${destination}/${filename}"
+            aws ssm put-parameter --name "/${type}/${destination}/${repo}" --type "String" --value "${sourcevalnoquotes}" --overwrite
+        fi
     done
 }
 
@@ -28,7 +34,7 @@ sha=$(sha256sum "${GITHUB_WORKSPACE}/${INPUT_BINARY_DIR}/${archive_filename}" | 
 aws configure set aws_access_key_id "${INPUT_AWS_ACCESS_KEY_ID}" || exit 1
 aws configure set aws_secret_access_key "${INPUT_AWS_SECRET_ACCESS_KEY}" || exit 1
 aws configure set region "${INPUT_AWS_REGION}" || exit 1
-copy_params sha staging unit-test || exit 1
+copy_params sha staging unit-test "${INPUT_BUCKET_PREFIX}" "${archive_filename}" || exit 1
 
 if [ -n "${INPUT_PROGRAM_NAME}" ]; then
     aws ssm put-parameter --name "/sha/unit-test/${INPUT_PROGRAM_NAME}" --type "String" --value "${sha}" --overwrite || exit 1
@@ -46,7 +52,7 @@ popd
 [ "${gotest_result}" -eq 0 ] || exit 1
 
 # Update staging version in parameter store
-copy_params sha unit-test staging || exit 1
+copy_params sha unit-test staging "${INPUT_BUCKET_PREFIX}" "${archive_filename}" || exit 1
 
 # Deploy to staging
 if [ -n "${INPUT_LIVE_DIR}" ]; then
