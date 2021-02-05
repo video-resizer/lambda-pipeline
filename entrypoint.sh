1aa7acda7f5569b38d968f00bf57264613e0d221
Keith McDonald <keithmmcdonald@gmail.com>
2021-02-05 01:09:05 -0800

    Remove another dependency on the option PROGRAM_NAME parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -euf -o pipefail
-set -x
 
 copy_params() {
-    type="${1}"
-    source="${2}"
-    destination="${3}"
-    bucketprefix="${4}"
+    source="${1}"
+    destination="${2}"
+    bucketprefix="${3}"
 
     eval "arr=( $(aws s3api list-objects --bucket "${bucketprefix}-${source}" --query 'Contents[].Key' | jq -r '[.[] | select(endswith(".zip"))] | @sh' ) )"
     for key in "${arr[@]}"
@@ -24,16 +22,14 @@ function gotest(){
     find $1 -name '*.go' | xargs -n1 -P1 go test -v -timeout 30m
 }
 
-# Update unit-test version in parameter store
-archive_filename="${INPUT_PROGRAM_NAME}.${INPUT_EXTENSION}"
-sha=$(openssl dgst -sha256 -binary "${GITHUB_WORKSPACE}/${INPUT_BINARY_DIR}/${archive_filename}" | openssl enc -base64)
-
 aws configure set aws_access_key_id "${INPUT_AWS_ACCESS_KEY_ID}" || exit 1
 aws configure set aws_secret_access_key "${INPUT_AWS_SECRET_ACCESS_KEY}" || exit 1
 aws configure set region "${INPUT_AWS_REGION}" || exit 1
-copy_params sha staging unit-test "${INPUT_BUCKET_PREFIX}" || exit 1
+copy_params staging unit-test "${INPUT_BUCKET_PREFIX}" || exit 1
 
 if [ -n "${INPUT_PROGRAM_NAME}" ]; then
+    archive_filename="${INPUT_PROGRAM_NAME}.${INPUT_EXTENSION}"
+    sha=$(openssl dgst -sha256 -binary "${GITHUB_WORKSPACE}/${INPUT_BINARY_DIR}/${archive_filename}" | openssl enc -base64)
     aws s3 cp "${GITHUB_WORKSPACE}/${INPUT_BINARY_DIR}/${archive_filename}" "s3://${bucketprefix}-unit-test/build_artifacts/${archive_filename}" --metadata "sha256=${sha}"
 fi
 
@@ -49,7 +45,7 @@ popd
 [ "${gotest_result}" -eq 0 ] || exit 1
 
 # Update staging version in parameter store
-copy_params sha unit-test staging "${INPUT_BUCKET_PREFIX}" || exit 1
+copy_params unit-test staging "${INPUT_BUCKET_PREFIX}" || exit 1
 
 # Deploy to staging
 if [ -n "${INPUT_LIVE_DIR}" ]; then
