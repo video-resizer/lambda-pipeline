412620e4bd09de016dc6b048ec5866f0548a8389
Keith McDonald <keithmmcdonald@gmail.com>
2021-02-05 00:56:16 -0800

    Remove dependency on optional PROGRAM_NAME parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -euf -o pipefail
+set -x
 
 copy_params() {
     type="${1}"
     source="${2}"
     destination="${3}"
     bucketprefix="${4}"
-    filename="${5}"
 
     eval "arr=( $(aws s3api list-objects --bucket "${bucketprefix}-${source}" --query 'Contents[].Key' | jq -r '[.[] | select(endswith(".zip"))] | @sh' ) )"
     for key in "${arr[@]}"
@@ -15,7 +15,7 @@ copy_params() {
         destinationval=$(aws s3api head-object --bucket "${bucketprefix}-${destination}" --key "${key}" | jq .Metadata.sha256)
         if [ "${sourceval}" != "${destinationval}" ]; then
             sourcevalnoquotes=$(echo "${sourceval}" | sed 's/"//g')
-            aws s3 cp "s3://${bucketprefix}-${source}/build_artifacts/${filename}" "s3://${bucketprefix}-${destination}/build_artifacts/${filename}" --metadata "sha256=${sourceval}"
+            aws s3 cp "s3://${bucketprefix}-${source}/${key}" "s3://${bucketprefix}-${destination}/${key}" --metadata "sha256=${sourceval}"
         fi
     done
 }
@@ -31,7 +31,7 @@ sha=$(openssl dgst -sha256 -binary "${GITHUB_WORKSPACE}/${INPUT_BINARY_DIR}/${ar
 aws configure set aws_access_key_id "${INPUT_AWS_ACCESS_KEY_ID}" || exit 1
 aws configure set aws_secret_access_key "${INPUT_AWS_SECRET_ACCESS_KEY}" || exit 1
 aws configure set region "${INPUT_AWS_REGION}" || exit 1
-copy_params sha staging unit-test "${INPUT_BUCKET_PREFIX}" "${archive_filename}" || exit 1
+copy_params sha staging unit-test "${INPUT_BUCKET_PREFIX}" || exit 1
 
 if [ -n "${INPUT_PROGRAM_NAME}" ]; then
     aws s3 cp "${GITHUB_WORKSPACE}/${INPUT_BINARY_DIR}/${archive_filename}" "s3://${bucketprefix}-unit-test/build_artifacts/${archive_filename}" --metadata "sha256=${sha}"
@@ -49,7 +49,7 @@ popd
 [ "${gotest_result}" -eq 0 ] || exit 1
 
 # Update staging version in parameter store
-copy_params sha unit-test staging "${INPUT_BUCKET_PREFIX}" "${archive_filename}" || exit 1
+copy_params sha unit-test staging "${INPUT_BUCKET_PREFIX}" || exit 1
 
 # Deploy to staging
 if [ -n "${INPUT_LIVE_DIR}" ]; then
